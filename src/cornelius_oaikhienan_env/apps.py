fa72b9f4ebf864f7ef249eec05aa5b7fa8fd451e
oaikhienancornelius97 <corneliusedos@gmail.com>
2022-06-02 18:28:21 +0100

    first commit

---
+++ src/cornelius_oaikhienan_env/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CorneliusOaikhienanEnvConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cornelius_oaikhienan_env'
